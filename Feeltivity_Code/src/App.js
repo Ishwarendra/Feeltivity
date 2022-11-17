b0a637e6f7d2621961cefed57c7580ae2008a60c
Adil <adilvinayak19@gmail.com>
2022-11-17 21:32:02 +0530

    Firebase-config setup

---
+++ Feeltivity_Code/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      Hello World!
+      Lets start
+    </div>
+  );
+}
+
+export default App;
---
+++ Feeltivity_Code/src/firebase-config.js
@@ -0,0 +1,13 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBgmd_rP-_AUzMncadigX19Sy8CyvguGQI",
+  authDomain: "feeltivity.firebaseapp.com",
+  projectId: "feeltivity",
+  storageBucket: "feeltivity.appspot.com",
+  messagingSenderId: "1049393684316",
+  appId: "1:1049393684316:web:9213c9e2f0ac0438d3eb99",
+  measurementId: "G-NRCL2VEMHJ"
+};
+
+const app = initializeApp(firebaseConfig);

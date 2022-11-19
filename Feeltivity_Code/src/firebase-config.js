4ba347976504c1ea1727df7d900e81e0ace6794d
Adil <adilvinayak19@gmail.com>
2022-11-19 17:31:46 +0530

    auth firestore storage setup

--- Feeltivity_Code/src/firebase-config.js
+++ Feeltivity_Code/src/firebase-config.js
@@ -1,4 +1,7 @@
 import { initializeApp } from "firebase/app";
+import {getFirestore} from 'firebase/firestore'
+import {getAuth} from 'firebase/auth'
+import {getStorage} from 'firebase/storage'
 
 const firebaseConfig = {
   apiKey: "AIzaSyBgmd_rP-_AUzMncadigX19Sy8CyvguGQI",
@@ -11,3 +14,6 @@ const firebaseConfig = {
 };
 
 const app = initializeApp(firebaseConfig);
+export const db=getFirestore(app);
+export const auth=getAuth(app)
+export const storage=getStorage(app)
--- Feeltivity_Code/src/page/Home/Home.jsx
+++ Feeltivity_Code/src/page/Home/Home.jsx
@@ -2,6 +2,8 @@ import React from 'react'
 
 export default function Home() {
   return (
-    <div>Home</div>
+    <div className='bg-indigo-500'>
+      Home
+    </div>
   )
 }

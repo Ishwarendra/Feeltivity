c103706f3e4efcca835ea817765bd321f49807df
Adil <adilvinayak19@gmail.com>
2022-11-19 17:44:38 +0530

    err fix

--- Feeltivity_Code/src/page/Home/Home.jsx
+++ Feeltivity_Code/src/page/Home/Home.jsx
@@ -2,15 +2,12 @@ import React from 'react'
 
 export default function Home() {
   return (
-<<<<<<< HEAD
     <div className='bg-indigo-500'>
       Home
-=======
     <div className=''>
       <p class="bg-red-300">
         aghaghg
       </p>
->>>>>>> 50023009b3eb5a1c5c0c8c39d46af64328e086de
     </div>
   )
 }

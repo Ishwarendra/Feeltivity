e36e7b1b01d6637024fb9bbea00a41f0ef4d4e59
Ishwarendra Jha <ishwarendra27@gmail.com>
2022-11-26 22:21:21 +0530

    about us card added

---
+++ Feeltivity_Code/src/components/ProfileCard/ProfileCard.jsx
@@ -0,0 +1,45 @@
+import React from "react";
+import LinkedInIcon from "@mui/icons-material/LinkedIn";
+import GitHubIcon from "@mui/icons-material/GitHub";
+
+export const ProfileCard = ({
+  name = "Your name",
+  subtitle = "Some one-liner about you",
+  content = "Main Content",
+  socialLinks = ["#", "#"] /* LinkedIn, Github */,
+  profileImg = "https://images.unsplash.com/photo-1568602471122-7832951cc4c5?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80",
+}) => {
+  return (
+    <div className="grid grid-cols-1 bg-[#202938] w-[90vw] sm:w-[45vw] md:w-[40vw] min-h-[300px] rounded-lg">
+      {/* Some Image */}
+      <div className="flex flex-wrap justify-center">
+        <div className="p-5">
+          <img
+            src={profileImg}
+            alt="..."
+            className="shadow rounded-full h-[200px] w-[200px] align-middle border-none"
+          />
+        </div>
+      </div>
+
+      {/* Name, Oneliner, content, social Links */}
+      <div className="mx-4">
+        <div className="font-semibold my-2">
+          <div className="text-[#f0f4f6]">{name}</div>
+          <div className="text-[#848df1]">{subtitle}</div>
+        </div>
+
+        <div className="text-white font-medium">{content}</div>
+
+        <div className="mb-4 mt-2 flex">
+          <div className='mr-1'>
+            <LinkedInIcon sx={{ color: "#9ca2ae" }} />
+          </div>
+          <div className='ml-1'>
+            <GitHubIcon sx={{ color: "#9ca2ae" }} />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
--- Feeltivity_Code/src/page/AboutUs/AboutUs.jsx
+++ Feeltivity_Code/src/page/AboutUs/AboutUs.jsx
@@ -1,16 +1,30 @@
-import React from 'react'
-import Navbar from '../../components/Navbar/Navbar'
+import React from "react";
+import Navbar from "../../components/Navbar/Navbar";
+import { ProfileCard } from "../../components/ProfileCard/ProfileCard";
 
 export default function AboutUs() {
   return (
-    <div>
+    <div className="min-h-screen bg-[#111828]">
       <div>
         <Navbar />
       </div>
 
-      <div>
-        About Us
+      <div className="font-bold text-4xl text-white p-5">Meet our team</div>
+
+      <div className="grid grid-cols-2 place-items-center">
+        <div className="w-auto h-auto m-5">
+          <ProfileCard
+            name="Dummy Name"
+            subtitle="Dummy Subtitle"
+            content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum facere ducimus expedita quibusdam vero obcaecati dolorem?"
+            socialLinks={["#", "#"]}
+          />
+          </div>
+
+          <div><ProfileCard /></div>
+          <div><ProfileCard /></div>
+          <div><ProfileCard /></div>
       </div>
     </div>
-  )
+  );
 }
